f8b7c09323cba93f3ec5085aecb36ac9e5e6acf6
rendykml <rendikamaluddin2018@gmail.com>
2025-12-01 20:00:36 +0700

    push

--- models/Location.js
+++ models/Location.js
@@ -4,12 +4,9 @@ const LocationSchema = new mongoose.Schema({
   name: { type: String, required: true },
   category: { type: String }, 
   description: { type: String },
-
-  // koordinat lokasi
   lat: { type: Number, required: true },
   lng: { type: Number, required: true },
-
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Location", LocationSchema);
+export default mongoose.model("Location", LocationSchema);
